56393f02abc4cbb9b6ea7946618082fc991d6d82
imadr <hi@imadr.me>
2024-06-05 18:25:51 +0200

    dawn working

--- main.cpp
+++ main.cpp
@@ -1,51 +1,10 @@
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu_cpp.h>
 #include <webgpu/webgpu_glfw.h>
-#include <webgpu/webgpu_glfw.h>
-#include <iostream>
 
-wgpu::Instance instance;
-wgpu::Device device;
-wgpu::RenderPipeline pipeline;
-wgpu::SwapChain swapChain;
+#include <cstdio>
 
-const uint32_t width = 512;
-const uint32_t height = 512;
-
-void SetupSwapChain(wgpu::Surface surface) {
-    wgpu::SwapChainDescriptor scDesc{.usage = wgpu::TextureUsage::RenderAttachment,
-                                     .format = wgpu::TextureFormat::BGRA8Unorm,
-                                     .width = width,
-                                     .height = height,
-                                     .presentMode = wgpu::PresentMode::Fifo};
-    swapChain = device.CreateSwapChain(surface, &scDesc);
-}
-
-void GetDevice(void (*callback)(wgpu::Device)) {
-    instance.RequestAdapter(
-        nullptr,
-        [](WGPURequestAdapterStatus status, WGPUAdapter cAdapter, const char* message,
-           void* userdata) {
-            if (status != WGPURequestAdapterStatus_Success) {
-                exit(0);
-            }
-            wgpu::Adapter adapter = wgpu::Adapter::Acquire(cAdapter);
-            adapter.RequestDevice(
-                nullptr,
-                [](WGPURequestDeviceStatus status, WGPUDevice cDevice, const char* message,
-                   void* userdata) {
-                    wgpu::Device device = wgpu::Device::Acquire(cDevice);
-                    device.SetUncapturedErrorCallback(
-                        [](WGPUErrorType type, const char* message, void* userdata) {
-                            std::cout << "Error: " << type << " - message: " << message;
-                        },
-                        nullptr);
-                    reinterpret_cast<void (*)(wgpu::Device)>(userdata)(device);
-                },
-                userdata);
-        },
-        reinterpret_cast<void*>(callback));
-}
+#include "dawn/native/DawnNative.h"
 
 const char shaderCode[] = R"(
 struct VertexOutput {
@@ -77,69 +36,134 @@ fn fs_main(input: VertexOutput) -> @location(0) vec4f {
 }
 )";
 
-void CreateRenderPipeline() {
-    wgpu::ShaderModuleWGSLDescriptor wgslDesc{};
-    wgslDesc.code = shaderCode;
+struct WebGpuRenderer {
+    std::unique_ptr<dawn::native::Instance> instance;
+    wgpu::BackendType backendType = wgpu::BackendType::Vulkan;
+    wgpu::AdapterType adapterType = wgpu::AdapterType::Unknown;
+    std::vector<std::string> enableToggles;
+    std::vector<std::string> disableToggles;
+    wgpu::SwapChain swapChain;
 
-    wgpu::ShaderModuleDescriptor shaderModuleDescriptor{.nextInChain = &wgslDesc};
-    wgpu::ShaderModule shaderModule = device.CreateShaderModule(&shaderModuleDescriptor);
+    wgpu::Device device;
+    wgpu::RenderPipeline pipeline;
 
-    wgpu::ColorTargetState colorTargetState{.format = wgpu::TextureFormat::BGRA8Unorm};
+    void init(GLFWwindow* window, uint32_t width, uint32_t height) {
+        WGPUInstanceDescriptor instanceDescriptor{};
+        instanceDescriptor.features.timedWaitAnyEnable = true;
+        instance = std::make_unique<dawn::native::Instance>(&instanceDescriptor);
+        wgpu::RequestAdapterOptions options = {};
+        options.backendType = backendType;
+        auto adapters = instance->EnumerateAdapters(&options);
+        wgpu::DawnAdapterPropertiesPowerPreference power_props{};
+        wgpu::AdapterProperties adapterProperties{};
+        adapterProperties.nextInChain = &power_props;
+        auto isAdapterType = [this, &adapterProperties](const auto& adapter) -> bool {
+            if (adapterType == wgpu::AdapterType::Unknown) {
+                return true;
+            }
+            adapter.GetProperties(&adapterProperties);
+            return adapterProperties.adapterType == adapterType;
+        };
+        auto preferredAdapter = std::find_if(adapters.begin(), adapters.end(), isAdapterType);
+        if (preferredAdapter == adapters.end()) {
+            fprintf(stderr, "Failed to find an adapter! Please try another adapter type.\n");
+            device = wgpu::Device();
+            return;
+        }
+        std::vector<const char*> enableToggleNames;
+        std::vector<const char*> disabledToggleNames;
+        for (const std::string& toggle : enableToggles) {
+            enableToggleNames.push_back(toggle.c_str());
+        }
+        for (const std::string& toggle : disableToggles) {
+            disabledToggleNames.push_back(toggle.c_str());
+        }
+        WGPUDawnTogglesDescriptor toggles;
+        toggles.chain.sType = WGPUSType_DawnTogglesDescriptor;
+        toggles.chain.next = nullptr;
+        toggles.enabledToggles = enableToggleNames.data();
+        toggles.enabledToggleCount = enableToggleNames.size();
+        toggles.disabledToggles = disabledToggleNames.data();
+        toggles.disabledToggleCount = disabledToggleNames.size();
+        WGPUDeviceDescriptor deviceDesc = {};
+        deviceDesc.nextInChain = reinterpret_cast<WGPUChainedStruct*>(&toggles);
+        WGPUDevice backendDevice = preferredAdapter->CreateDevice(&deviceDesc);
+        DawnProcTable backendProcs = dawn::native::GetProcs();
+        auto surfaceChainedDesc = wgpu::glfw::SetupWindowAndGetSurfaceDescriptor(window);
+        WGPUSurfaceDescriptor surfaceDesc;
+        surfaceDesc.label = "surface";
+        surfaceDesc.nextInChain = reinterpret_cast<WGPUChainedStruct*>(surfaceChainedDesc.get());
+        WGPUSurface surface = backendProcs.instanceCreateSurface(instance->Get(), &surfaceDesc);
+        WGPUSwapChainDescriptor swapChainDesc = {};
+        swapChainDesc.usage = WGPUTextureUsage_RenderAttachment;
+        swapChainDesc.format = static_cast<WGPUTextureFormat>(wgpu::TextureFormat::BGRA8Unorm);
+        swapChainDesc.width = width;
+        swapChainDesc.height = height;
+        swapChainDesc.presentMode = WGPUPresentMode_Mailbox;
+        WGPUSwapChain backendSwapChain =
+            backendProcs.deviceCreateSwapChain(backendDevice, surface, &swapChainDesc);
 
-    wgpu::FragmentState fragmentState{
-        .module = shaderModule, .targetCount = 1, .targets = &colorTargetState};
+        WGPUDevice cDevice = nullptr;
+        DawnProcTable procs;
 
-    wgpu::RenderPipelineDescriptor descriptor{.vertex = {.module = shaderModule},
-                                              .fragment = &fragmentState};
-    pipeline = device.CreateRenderPipeline(&descriptor);
-}
+        procs = backendProcs;
+        cDevice = backendDevice;
+        swapChain = wgpu::SwapChain::Acquire(backendSwapChain);
 
-void Render() {
-    wgpu::RenderPassColorAttachment attachment{.view = swapChain.GetCurrentTextureView(),
-                                               .loadOp = wgpu::LoadOp::Clear,
-                                               .storeOp = wgpu::StoreOp::Store};
+        device = wgpu::Device::Acquire(cDevice);
 
-    wgpu::RenderPassDescriptor renderpass{.colorAttachmentCount = 1,
-                                          .colorAttachments = &attachment};
+        /////////////
+        wgpu::ShaderModuleWGSLDescriptor wgslDesc{};
+        wgslDesc.code = shaderCode;
 
-    wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
-    wgpu::RenderPassEncoder pass = encoder.BeginRenderPass(&renderpass);
-    pass.SetPipeline(pipeline);
-    pass.Draw(3);
-    pass.End();
-    wgpu::CommandBuffer commands = encoder.Finish();
-    device.GetQueue().Submit(1, &commands);
-}
+        wgpu::ShaderModuleDescriptor shaderModuleDescriptor{.nextInChain = &wgslDesc};
+        wgpu::ShaderModule shaderModule = device.CreateShaderModule(&shaderModuleDescriptor);
 
-void InitGraphics(wgpu::Surface surface) {
-    SetupSwapChain(surface);
-    CreateRenderPipeline();
-}
+        wgpu::ColorTargetState colorTargetState{.format = wgpu::TextureFormat::BGRA8Unorm};
 
-void Start() {
-    if (!glfwInit()) {
-        return;
+        wgpu::FragmentState fragmentState{
+            .module = shaderModule, .targetCount = 1, .targets = &colorTargetState};
+
+        wgpu::RenderPipelineDescriptor descriptor{.vertex = {.module = shaderModule},
+                                                  .fragment = &fragmentState};
+        pipeline = device.CreateRenderPipeline(&descriptor);
     }
 
+    void draw() {
+        wgpu::RenderPassColorAttachment attachment{.view = swapChain.GetCurrentTextureView(),
+                                                   .loadOp = wgpu::LoadOp::Clear,
+                                                   .storeOp = wgpu::StoreOp::Store};
+
+        wgpu::RenderPassDescriptor renderpass{.colorAttachmentCount = 1,
+                                              .colorAttachments = &attachment};
+
+        wgpu::CommandEncoder encoder = device.CreateCommandEncoder();
+        wgpu::RenderPassEncoder pass = encoder.BeginRenderPass(&renderpass);
+        pass.SetPipeline(pipeline);
+        pass.Draw(3);
+        pass.End();
+        wgpu::CommandBuffer commands = encoder.Finish();
+        device.GetQueue().Submit(1, &commands);
+
+        swapChain.Present();
+        dawn::native::InstanceProcessEvents(instance->Get());
+    }
+};
+
+int main() {
+    WebGpuRenderer renderer;
+
+    glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    const uint32_t width = 512;
+    const uint32_t height = 512;
     GLFWwindow* window = glfwCreateWindow(width, height, "webgpu-test", nullptr, nullptr);
 
-    wgpu::Surface surface = wgpu::glfw::CreateSurfaceForWindow(instance, window);
-
-    InitGraphics(surface);
+    renderer.init(window, width, height);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-        Render();
-        swapChain.Present();
-        instance.ProcessEvents();
+        renderer.draw();
     }
-}
-
-int main() {
-    instance = wgpu::CreateInstance();
-    GetDevice([](wgpu::Device dev) {
-        device = dev;
-        Start();
-    });
 }
